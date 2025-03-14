a9e127a020379d4870cc1b53e30fbd62b3daf556
yujuCho <11146017@ntub.edu.tw>
2025-03-14 13:37:33 +0800

    connent_exisiting_page
    我把目前有的所有頁面都和在一起囉！

--- android/app/src/main/kotlin/woundDoctor/example/main_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package woundDoctor.example.main_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:main_project/main.dart';
+import 'package:wounddetection/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const RegistrationScreen());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
