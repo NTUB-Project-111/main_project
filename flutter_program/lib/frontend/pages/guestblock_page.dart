77f8992619da1ee732f96dee8f3c05713aef7077
yujuCho <11146017@ntub.edu.tw>
2025-07-19 17:16:50 +0800

    #108 guestblock_revise
    guestblock修改OK

--- flutter_program/lib/frontend/pages/guestblock_page.dart
+++ flutter_program/lib/frontend/pages/guestblock_page.dart
@@ -1,3 +1,4 @@
+import 'package:drw/frontend/pages/login_page.dart';
 import 'package:drw/frontend/utility/front_util.dart';
 import 'package:flutter/material.dart';
 import 'registerpages/disclaimer_page.dart';
@@ -14,6 +15,19 @@ class _GuestBlockPage extends State<GuestBlockPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFEBFEFF),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFEBFEFF),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: FrontUtil.textColor,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -59,10 +73,35 @@ class _GuestBlockPage extends State<GuestBlockPage> {
                 ),
               ],
             ),
+            // const SizedBox(height: 5),
+            // Text(
+            //   '(護理提醒、紀錄冊、我的)',
+            //   style: TextStyle(color: FrontUtil.textColor, fontSize: 14),
+            // ),
             const SizedBox(height: 5),
-            Text(
-              '(護理提醒、紀錄冊、我的)',
-              style: TextStyle(color: FrontUtil.textColor, fontSize: 14),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                );
+              },
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                minimumSize: const Size(0, 0),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              child: Text(
+                '已有帳號? 點我登入!',
+                style: TextStyle(
+                  color: FrontUtil.textColor,
+                  fontSize: 14,
+                  // decoration: TextDecoration.underline,
+                  decorationColor: FrontUtil.textColor,
+                ),
+              ),
             ),
           ],
         ),
