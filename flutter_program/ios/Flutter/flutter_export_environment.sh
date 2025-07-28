dc59aa5acf326c71f12c65c7b4c40ef20835bea8
YanjiaWong <11146024@ntub.edu.tw>
2025-07-28 15:43:42 +0800

    更新程式碼

--- flutter_program/ios/Flutter/flutter_export_environment.sh
+++ flutter_program/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_windows\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\github\main_project\flutter_program"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sally\Documents\GitHub\main_project\flutter_program"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
