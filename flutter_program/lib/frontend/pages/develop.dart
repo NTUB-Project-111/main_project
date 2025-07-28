6c04138c1f1fa5f5652bcb49ffb6e6612c35cc11
yujuCho <11146017@ntub.edu.tw>
2025-07-28 15:01:58 +0800

    #140 mine_moresets_jumppage
    『我的』的「更多設定」跳轉頁面。

---
+++ flutter_program/lib/frontend/pages/develop.dart
@@ -0,0 +1,56 @@
+import 'package:drw/frontend/pages/login_page.dart';
+import 'package:drw/frontend/utility/front_util.dart';
+import 'package:flutter/material.dart';
+import 'registerpages/disclaimer_page.dart';
+
+class DevelopPage extends StatefulWidget {
+  const DevelopPage({super.key});
+
+  @override
+  State<DevelopPage> createState() => _DevelopPage();
+}
+
+class _DevelopPage extends State<DevelopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEBFEFF),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFEBFEFF),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: FrontUtil.textColor,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/doctor_bear.png', width: 200),
+            const SizedBox(height: 13),
+            Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Text(
+                  '此功能正在開發中...敬啟期待',
+                  style: TextStyle(
+                    color: FrontUtil.textColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
