d22be629d62cde79254a7bf7fc1fd895bb71b5e4
yujuCho <11146017@ntub.edu.tw>
2025-07-28 17:50:48 +0800

    #144 debug_report
    診斷報告修正

--- flutter_program/lib/frontend/pages/report_page.dart
+++ flutter_program/lib/frontend/pages/report_page.dart
@@ -1,16 +1,109 @@
+// import 'package:drw/backend/models/report_model.dart';
+// import 'package:drw/backend/provider/user_provider.dart';
+// import 'package:drw/backend/provider/report_analyzer.dart';
+// import 'package:drw/frontend/headers/header5.dart';
+// import 'package:drw/frontend/pages/reportpages/button_part.dart';
+// import 'package:drw/frontend/pages/reportpages/care_part.dart';
+// import 'package:drw/frontend/pages/reportpages/hospital_part.dart';
+// import 'package:drw/frontend/pages/reportpages/record_part.dart';
+// import 'package:drw/frontend/pages/reportpages/title_part.dart';
+// import 'package:drw/frontend/pages/reportpages/wound_part.dart';
+// import 'package:drw/frontend/utility/front_util.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// class ReportPage extends StatefulWidget {
+//   final bool isExtra;
+//   final int? id;
+//   final String? oktime;
+//   final String? date;
+//   final String? woundType;
+
+//   const ReportPage({
+//     super.key,
+//     required this.isExtra,
+//     this.id,
+//     this.oktime,
+//     this.date,
+//     this.woundType,
+//   });
+
+//   @override
+//   State<ReportPage> createState() => _ReportPageState();
+// }
+
+// class _ReportPageState extends State<ReportPage> {
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     WidgetsBinding.instance.addPostFrameCallback((_) async {
+//       final report = Provider.of<Report>(context, listen: false);
+//       final userProvider = Provider.of<UserProvider>(context, listen: false);
+//       final analyzer = Provider.of<ReportAnalyzer>(context, listen: false);
+
+//       await analyzer.loadData(asyncCallback: () async {
+//         final isGuest = userProvider.isGuest;
+//         final user = userProvider.user;
+
+//         await report.loadData(
+//           isGuest ? "2000" : (user?.birthday ?? "2000"),
+//           isGuest ? "無" : (user?.disease ?? "無"),
+//           isGuest ? "每天" : (user?.freq ?? "每天"),
+//           widget.isExtra,
+//           widget.oktime,
+//           widget.date,
+//           widget.woundType,
+//         );
+//       });
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final isGuest = Provider.of<UserProvider>(context).isGuest;
+//     final isLoading = Provider.of<ReportAnalyzer>(context).isLoading;
+
+//     return Scaffold(
+//       backgroundColor: const Color(0xFFEBFEFF),
+//       body: isLoading
+//           ? Center(child: FrontUtil.loading())
+//           : SingleChildScrollView(
+//               child: Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 15),
+//                 child: Column(
+//                   children: [
+//                     const Header5(),
+//                     const TitlePart(),
+//                     const WoundPart(),
+//                     const CarePart(),
+//                     const HospitalPart(),
+//                     if (!isGuest) const RecordPart(),
+//                     if (!isGuest)
+//                       ButtonPart(
+//                         isExtra: widget.isExtra,
+//                         id: widget.id,
+//                       ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//     );
+//   }
+// }
+
 import 'package:drw/backend/models/report_model.dart';
 import 'package:drw/backend/provider/user_provider.dart';
-import 'package:drw/backend/provider/report_analyzer.dart';
 import 'package:drw/frontend/headers/header5.dart';
 import 'package:drw/frontend/pages/reportpages/button_part.dart';
 import 'package:drw/frontend/pages/reportpages/care_part.dart';
 import 'package:drw/frontend/pages/reportpages/hospital_part.dart';
 import 'package:drw/frontend/pages/reportpages/record_part.dart';
-import 'package:drw/frontend/pages/reportpages/title_part.dart';
 import 'package:drw/frontend/pages/reportpages/wound_part.dart';
 import 'package:drw/frontend/utility/front_util.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:drw/frontend/pages/reportpages/title_part.dart';
 
 class ReportPage extends StatefulWidget {
   final bool isExtra;
@@ -18,76 +111,78 @@ class ReportPage extends StatefulWidget {
   final String? oktime;
   final String? date;
   final String? woundType;
-
-  const ReportPage({
-    super.key,
-    required this.isExtra,
-    this.id,
-    this.oktime,
-    this.date,
-    this.woundType,
-  });
+  const ReportPage(
+      {super.key,
+      required this.isExtra,
+      this.id,
+      this.oktime,
+      this.date,
+      this.woundType});
 
   @override
   State<ReportPage> createState() => _ReportPageState();
 }
 
 class _ReportPageState extends State<ReportPage> {
+  // bool _analyzed = false;
+
+  // @override
+  // void didChangeDependencies() {
+  //   super.didChangeDependencies();
+  //   // 確保只分析一次
+  //   if (!_analyzed) {
+  //     final report = Provider.of<Report>(context, listen: false);
+  //     report.loadData();
+  //     _analyzed = true;
+  //   }
+  // }
+
   @override
   void initState() {
     super.initState();
-
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       final report = Provider.of<Report>(context, listen: false);
       final userProvider = Provider.of<UserProvider>(context, listen: false);
-      final analyzer = Provider.of<ReportAnalyzer>(context, listen: false);
-
-      await analyzer.loadData(asyncCallback: () async {
-        final isGuest = userProvider.isGuest;
-        final user = userProvider.user;
-
-        await report.loadData(
-          isGuest ? "2000" : (user?.birthday ?? "2000"),
-          isGuest ? "無" : (user?.disease ?? "無"),
-          isGuest ? "每天" : (user?.freq ?? "每天"),
+      final user = userProvider.user;
+      report.isLoading = true; // <-- 這行很關鍵！每次都要先設為 loading
+      await report.loadData(
+          user!.birthday,
+          user.disease,
+          user.freq,
           widget.isExtra,
           widget.oktime,
           widget.date,
-          widget.woundType,
-        );
-      });
+          widget.woundType); // 這樣 Consumer 才會觸發 CircularProgressIndicator
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final isGuest = Provider.of<UserProvider>(context).isGuest;
-    final isLoading = Provider.of<ReportAnalyzer>(context).isLoading;
-
-    return Scaffold(
-      backgroundColor: const Color(0xFFEBFEFF),
-      body: isLoading
-          ? Center(child: FrontUtil.loading())
-          : SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Column(
-                  children: [
-                    const Header5(),
-                    const TitlePart(),
-                    const WoundPart(),
-                    const CarePart(),
-                    const HospitalPart(),
-                    if (!isGuest) const RecordPart(),
-                    if (!isGuest)
-                      ButtonPart(
-                        isExtra: widget.isExtra,
-                        id: widget.id,
-                      ),
-                  ],
-                ),
+    return Consumer<Report>(
+      builder: (context, report, _) {
+        if (report.isLoading) {
+          return Scaffold(
+            body: Center(child: FrontUtil.loading()),
+          );
+        }
+        return Scaffold(
+            backgroundColor: const Color(0xFFEBFEFF),
+            body: SingleChildScrollView(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: [
+                  const Header5(),
+                  const TitlePart(),
+                  const WoundPart(),
+                  const CarePart(),
+                  const HospitalPart(),
+                  const RecordPart(),
+                  ButtonPart(isExtra: widget.isExtra, id: widget.id)
+                ],
               ),
-            ),
+            )));
+      },
     );
   }
 }
