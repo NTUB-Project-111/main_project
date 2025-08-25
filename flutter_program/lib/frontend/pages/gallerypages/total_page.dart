4499c94a391d3c573037e1886e191a2b0e32abcb
yujuCho <11146017@ntub.edu.tw>
2025-08-25 23:44:06 +0800

    #223 total_page_revise
    當年度所有傷口畫面調整，傷口照片位置。

--- flutter_program/lib/frontend/pages/gallerypages/total_page.dart
+++ flutter_program/lib/frontend/pages/gallerypages/total_page.dart
@@ -1,8 +1,8 @@
 import 'package:drw/backend/models/report.dart';
 import 'package:drw/backend/provider/report_provider.dart';
-// import 'package:drw/backend/services/apibase.dart';
 import 'package:drw/frontend/headers/header3.dart';
 import 'package:drw/frontend/pages/gallerypages/showreport_page.dart';
+import 'package:drw/frontend/utility/front_util.dart';
 import 'package:drw/frontend/pages/remind_page.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -19,6 +19,7 @@ class TotalPage extends StatefulWidget {
 
 class _TotalPageState extends State<TotalPage> {
   late Map<String, List<UserReport>> monthlyReports;
+  late String year;
 
   @override
   void initState() {
@@ -28,6 +29,13 @@ class _TotalPageState extends State<TotalPage> {
 
   void _groupImagesByMonth() {
     monthlyReports = {};
+    if (widget.yearlyReports.isNotEmpty) {
+      // 取出第一筆的年份 (因為這頁就是單一年份的紀錄)
+      year = widget.yearlyReports.first.date.substring(0, 4);
+    } else {
+      year = "";
+    }
+
     for (UserReport report in widget.yearlyReports) {
       DateTime date = DateTime.parse(report.date);
       String monthKey = DateFormat('MM月').format(date);
@@ -67,10 +75,12 @@ class _TotalPageState extends State<TotalPage> {
                     color: Color(0xFF04555D),
                   ),
                 ),
-                const Text(
-                  "2025年",
-                  style: TextStyle(
-                      color: Color(0xFF04555D), fontWeight: FontWeight.bold, fontSize: 14),
+                Text(
+                  "$year年",
+                  style: const TextStyle(
+                      color: Color(0xFF04555D),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14),
                 ),
                 const SizedBox(),
               ],
@@ -80,11 +90,14 @@ class _TotalPageState extends State<TotalPage> {
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: Padding(
-                padding: const EdgeInsets.only(left: 10, top: 10),
+                padding: const EdgeInsets.only(left: 10, top: 10, right: 10),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    for (var month in monthlyReports.keys.toList()..sort())
+                    // 月份由新到舊排序
+                    for (var month
+                        in monthlyReports.keys.toList()
+                          ..sort((a, b) => b.compareTo(a)))
                       _buildMonth(month, monthlyReports[month]!),
                   ],
                 ),
@@ -100,58 +113,103 @@ class _TotalPageState extends State<TotalPage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          month,
-          style: const TextStyle(
-            height: 3,
-            fontSize: 14,
-            color: Color(0xFF589399),
-            fontWeight: FontWeight.bold,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            month,
+            style: const TextStyle(
+              height: 3,
+              fontSize: 14,
+              color: Color(0xFF589399),
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        Wrap(
-          children: records.map((record) => _buildImage(record.photo, record)).toList(),
+        GridView.count(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          crossAxisCount: 4, // 一列4張
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          children: records.map((record) => _buildImage(record)).toList(),
         ),
       ],
     );
   }
 
-  Widget _buildImage(String imageUrl, UserReport userReport) {
+  // Widget _buildImage(UserReport userReport) {
+  //   return GestureDetector(
+  //     child: Container(
+  //       decoration: BoxDecoration(
+  //         color: Colors.grey,
+  //         borderRadius: BorderRadius.circular(10),
+  //       ),
+  //       child: ClipRRect(
+  //         borderRadius: BorderRadius.circular(10),
+  //         child: Image.network(
+  //           userReport.photo.toString(),
+  //           fit: BoxFit.cover,
+  //         ),
+  //       ),
+  //     ),
+  //     onTap: () {
+  //       final extra = isExtra(userReport);
+  //       Navigator.push(
+  //           context,
+  //           MaterialPageRoute(
+  //               builder: (context) => ShowReportPage(
+  //                     report: userReport,
+  //                     isExtra: extra,
+  //                   )));
+  //     },
+  //   );
+  // }
+
+  Widget _buildImage(UserReport userReport) {
     return GestureDetector(
       child: Container(
-        margin: const EdgeInsets.only(right: 10, bottom: 10),
         decoration: BoxDecoration(
           color: Colors.grey,
           borderRadius: BorderRadius.circular(10),
         ),
-        height: 82,
-        width: 82,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(10),
           child: Image.network(
-            imageUrl.toString(),
-            width: 82,
-            height: 82,
+            userReport.photo.toString(),
             fit: BoxFit.cover,
           ),
-          // child: Image.network(
-          //   Uri.parse(ApiBase.baseUrl).resolve(imageUrl).toString(),
-          //   width: 82,
-          //   height: 82,
-          //   fit: BoxFit.cover,
-          // ),
         ),
       ),
       onTap: () {
         final extra = isExtra(userReport);
         Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ShowReportPage(
-                      report: userReport,
-                      isExtra: extra,
-                    )));
+          context,
+          MaterialPageRoute(
+            builder: (context) => ShowReportPage(
+              report: userReport,
+              isExtra: extra,
+            ),
+          ),
+        );
       },
+      // onLongPress: () {
+      //   FrontUtil.showConfirmDialog(
+      //     context,
+      //     Colors.red,
+      //     "確定要刪除此紀錄嗎？",
+      //     null,
+      //     "取消",
+      //     "刪除",
+      //     () {
+      //       context.read<ReportProvider>().deleteReport(userReport.id);
+      //       FrontUtil.showSuccess('紀錄已刪除!');
+      //       setState(() {
+      //         _groupImagesByMonth(); // 重新整理 UI
+      //       });
+      //     },
+      //   );
+      // },
     );
   }
 
